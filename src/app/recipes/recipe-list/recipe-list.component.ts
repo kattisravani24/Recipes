2c01c647ea477f933f5da200c2b32d2c843c19b8
kattisravni24 <kattisravni24@gmail.com>
2020-04-19 00:33:26 +0530

    latest commits

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
     new Recipe('PaniPuri', 'Life', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIXswEuhcMeTu2RpTwtwJgEx-sAP0TXIK_C1mn63WVE7kQKkCg&usqp=CAU'),
-    new Recipe('PaniPuri', 'Life', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIXswEuhcMeTu2RpTwtwJgEx-sAP0TXIK_C1mn63WVE7kQKkCg&usqp=CAU'),
-    new Recipe('PaniPuri', 'Life', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIXswEuhcMeTu2RpTwtwJgEx-sAP0TXIK_C1mn63WVE7kQKkCg&usqp=CAU'),
-    new Recipe('PaniPuri', 'Life', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIXswEuhcMeTu2RpTwtwJgEx-sAP0TXIK_C1mn63WVE7kQKkCg&usqp=CAU')
+    new Recipe('PaniPuri Two', 'Life', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIXswEuhcMeTu2RpTwtwJgEx-sAP0TXIK_C1mn63WVE7kQKkCg&usqp=CAU')
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
